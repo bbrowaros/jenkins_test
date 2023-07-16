0eea7584dfce8b382e3535286e23f1e805540fa1
bbrowaros <bbrowaros@gmail.com>
2023-07-16 16:25:51 +0200

    test2

--- test_script.sh
+++ test_script.sh
@@ -1,8 +1,9 @@
-!#/bin/bash
+#!/bin/bash
 
 echo "this is a test script for a jenkins integration" 
 
-for i in $(cat ./list); do 
-   echo ${i} 
-done
+while IFS= read -r line
+do
+   echo "${line}" 
+done  < list
 
